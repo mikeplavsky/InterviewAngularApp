a4fa2d2a11d8ebefe42b1439d95e4a83df1ed461
Pavel Timofeev <pavel.timofeev@quest.com>
2020-02-07 13:54:08 +0300

    Remove implementation

--- simpleApp/src/app/components/widget/widget.component.ts
+++ simpleApp/src/app/components/widget/widget.component.ts
@@ -1,20 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
-import { DataService } from 'src/app/services/data.service';
-import { DataSource } from 'src/app/components/chart/chart.component';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-widget',
   templateUrl: './widget.component.html',
   styleUrls: ['./widget.component.css']
 })
-export class WidgetComponent implements OnInit {
+export class WidgetComponent {
 
-  $data: Observable<DataSource>;
-
-  constructor(public ds: DataService) { }
-
-  ngOnInit() {
-    this.$data = this.ds.$accountsStatistics();
-  }
 }
--- simpleApp/src/app/services/data.service.ts
+++ simpleApp/src/app/services/data.service.ts
@@ -1,28 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { forkJoin } from 'rxjs';
-import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  constructor(private http: HttpClient) { }
-
-  public $accountsStatistics() {
-    
-    return forkJoin(
-      this.http.get('./assets/account1.json'),
-      this.http.get('./assets/account2.json')
-    )
-    .pipe( 
-      map( (res:any) => {
-        return {
-          account1: res[0].response,
-          account2: res[1].activity.map( x => x.data )
-        }
-      })
-    )
-  }
 }
